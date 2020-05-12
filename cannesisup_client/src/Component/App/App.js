137ae0e992f5266772c10694225bcbbe1403c462
Manon de Saint-Ferreol <manon.vial@gmail.com>
2020-05-12 12:16:31 +0200

    creation dossiers et router

---
+++ cannesisup_client/src/Component/App/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import SignUp from "../SignUp/SignUp";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          {/* Sign up route */}
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+
+          {/* Annuaire route */}
+          <Route path="/annuaire"></Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;

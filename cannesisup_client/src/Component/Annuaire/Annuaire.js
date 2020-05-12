43d19ec2f0c5099451ed104a5cd1831b22b3a76b
Manon de Saint-Ferreol <manon.vial@gmail.com>
2020-05-12 17:30:59 +0200

    modele annuaire responsive

---
+++ cannesisup_client/src/Component/Annuaire/Annuaire.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+
+class Annuaire extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              1
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              2
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              3
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              4
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              1
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              2
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              3
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              4
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              1
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              2
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              3
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              4
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              1
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              2
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              3
+            </Col>
+            <Col className="style1" xs={12} sm={6} md={4}>
+              4
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Annuaire;
--- cannesisup_client/src/Component/SignUp/SignUp.js
+++ cannesisup_client/src/Component/SignUp/SignUp.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+
 import "./style.css";
 
 class SignUp extends Component {
   render() {
-    return <div className="test">Sign up Component</div>;
+    return <div></div>;
   }
 }
 

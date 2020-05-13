7d005b12ce5923e402f0a9d2ebef2c09463f5c68
Maxime FABRE <maxusdm06@gmail.com>
2020-05-13 10:30:29 +0200

    Creation modele + squelette essai bdd

---
+++ cannesisup_server/models/adherent.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+// const uniqueValidator = require("mongoose-unique-validator"); TODO A ACTIVER
+
+const adherentSchema = mongoose.Schema({
+  nomDeSociete: String,
+  logo: String,
+  descriptionExhaustive: String,
+  secteurDactivite: String,
+  mailPrive: { type: String, unique: true },
+  motDePasse: String,
+
+  dirigeant: {
+    nom: String,
+    prenom: String,
+    paroleDeMembre: String,
+  },
+
+  coordonnes: {
+    adresse: String,
+    mailSociete: String,
+    telephone: String,
+    siteWeb: String,
+  },
+
+  reseauSociaux: {
+    facebook: String,
+    instagram: String,
+    twitter: String,
+    linkedin: String,
+  },
+
+  estActif: Boolean,
+
+  pdf: String,
+});
+
+/* avec unique verifie que 2 utilisateurs n'ont pas la même adresse mail */
+
+// userSchema.plugin(uniqueValidator); TODO A ACTIVER
+
+/* Model */
+const Adherent = mongoose.model("Adherent", adherentSchema);
+
+module.exports = Adherent;

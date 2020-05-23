ad54616610313970d2b7ebc27106dc9c9a83bc83
Manon de Saint-Ferreol <manon.vial@gmail.com>
2020-05-23 12:26:28 +0200

    ajout supp files

--- cannesisup_server/controllers/updateAdherent.js
+++ cannesisup_server/controllers/updateAdherent.js
@@ -1,9 +1,34 @@
 const Adherent = require("../models/adherent");
+const fs = require("fs");
+const path = require("path");
 
 const updateAdherent = (req, res, next) => {
-  console.log(req.files.couv[0]);
+  Adherent.findOne({ _id: req.params.id }, (err, adherent) => {
+    if (!adherent) {
+      res.status(400).json({ msg: "no adherent in DDB" });
+    }
 
-  if (req.files.couv) {
+    /* Suppression de l'ancien fichier */
+    if (adherent.photoCouverture) {
+      const filepath =
+        path.join(__dirname, "../public/uploads/") + adherent.photoCouverture;
+
+      fs.unlink(filepath, (err) => {
+        if (err) throw err;
+        console.log("file deleted");
+      });
+    }
+
+    /* Ajout nouveau fichier */
+    Adherent.findOneAndUpdate(
+      { _id: req.params.id },
+      { photoCouverture: req.files.couv[0].filename }
+    )
+      .then(() => res.status(200).json({ message: "Objet modifié !" }))
+      .catch((error) => res.status(400).json({ error }));
+  });
+
+  /*if (req.files.couv) {
     Adherent.findOneAndUpdate(
       { _id: req.params.id },
       { photoCouverture: req.files.couv[0].filename }
@@ -31,7 +56,7 @@ const updateAdherent = (req, res, next) => {
     )
       .then(() => res.status(200).json({ message: "Objet modifié !" }))
       .catch((error) => res.status(400).json({ error }));
-  }
+  }*/
 };
 
 module.exports = updateAdherent;
